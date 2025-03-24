74e2c6d931d1f46710f354409ff55c191ea61cd5
skkim <seungkyukim.dev@gmail.com>
2025-03-25 01:23:53 +0900

    feat: notion form 적용

--- frontend/src/modules/Home/components/CertificateModal.tsx
+++ frontend/src/modules/Home/components/CertificateModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Modal, Box, Typography, TextField, Button } from '@mui/material';
+import { Modal, Box, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 interface Props {
   open: boolean;
@@ -7,19 +8,43 @@ interface Props {
 }
 
 const CertificateModal: React.FC<Props> = ({ open, onClose }) => {
-  const handleSubmit = () => {
-    // TODO: Submit logic
-    onClose();
-  };
-
   return (
     <Modal open={open} onClose={onClose}>
       <Box sx={modalStyle}>
-        <Typography variant="h6" mb={2}>수료증 발급 신청 및 출력</Typography>
-        <TextField label="Name" fullWidth margin="normal" />
-        <TextField label="Email" fullWidth margin="normal" />
-        <TextField label="참여한 스터디" fullWidth margin="normal" />
-        <Button variant="contained" fullWidth onClick={handleSubmit} sx={{ mt: 2 }}>Submit</Button>
+        {/* Close Button */}
+        <IconButton
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            top: 8,
+            right: 8,
+            zIndex: 10,
+            color: 'grey.500',
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+
+        {/* Notion Embed */}
+        <Box
+          sx={{
+            width: '100%',
+            height: '600px',
+            borderRadius: '8px',
+            overflow: 'hidden',
+          }}
+        > 
+          <iframe
+            src="https://bailandoys.notion.site/ebd/1c03a2a2eed580b7ad6ac8c431061111"
+            width="100%"
+            height="600"
+            allowFullScreen
+            title="수료증 발급 신청 Notion Form"
+            style={{
+              border: 'none',
+            }}
+          />
+        </Box>
       </Box>
     </Modal>
   );
@@ -30,7 +55,8 @@ const modalStyle = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: '80%',
+  maxWidth: 800,
   bgcolor: 'background.paper',
   boxShadow: 24,
   p: 4,
