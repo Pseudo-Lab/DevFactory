3a4ec81214a5396626a94edfd22085a477bbc0aa
skkim.a <skkim.a@imagoworks.ai>
2025-03-24 17:17:32 +0900

    feat: add react router dom with logo link to homepage

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -3,6 +3,7 @@ import { AppBar, Toolbar, Typography, IconButton, Box } from '@mui/material';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import { useTheme } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
 import { useColorMode } from '../../styles/ThemeContext';
 
 interface HeaderProps {
@@ -18,10 +19,12 @@ const Header: React.FC<HeaderProps> = ({ logoSrc }) => {
    enableColorOnDark>
       <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
         <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <img src={logoSrc} alt="Logo" style={{ height: 40, marginRight: 16 }} />
-          <Typography variant="h6" component="div" sx={{ color: 'black', fontWeight: 'bold' }}>
-            Pseudolab ToolBox
-          </Typography>
+          <Link to="/" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none' }}>
+            <img src={logoSrc} alt="Logo" style={{ height: 40, marginRight: 16 }} />
+            <Typography variant="h6" sx={{ color: 'black', fontWeight: 'bold' }}>
+              PseudoLab ToolBox
+            </Typography>
+          </Link>
           {/* <Typography variant="h4" align="center" gutterBottom>
             PseudoLab ToolBox
           </Typography> */}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ColorModeProvider } from './styles/ThemeContext';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ColorModeProvider>
-      <App />
-    </ColorModeProvider>
-  </React.StrictMode>,
+    <BrowserRouter>
+      <ColorModeProvider>
+        <App />
+      </ColorModeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
