0c62f5a925180d57baaef3fa76cf576ac806a09b
skkim <seungkyukim.dev@gmail.com>
2025-09-16 23:58:00 +0900

    chore: 안쓰는 거 삭제

--- cert/frontend/src/components/common/Footer.tsx
+++ cert/frontend/src/components/common/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Container, Grid, Typography, Link } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import { colors } from '../../styles/theme';
 
 const Footer: React.FC = () => {
