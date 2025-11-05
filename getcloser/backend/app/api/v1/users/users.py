9a171c6575743570ef87009efe558f3a8df026d2
hu6r1s <darmaa333@gmail.com>
2025-11-05 19:22:14 +0900

    refactor(getcloser): manage authorization processing globally

--- getcloser/backend/app/api/v1/users/users.py
+++ getcloser/backend/app/api/v1/users/users.py
@@ -1,24 +1,16 @@
 from core.database import get_db
-from core.dependencies import get_current_user
 from fastapi import APIRouter, Depends
 from models.users import User
-from services.user_service import auth_user, get_user, parse_user_id
+from services.user_service import  get_user, parse_user_id
 from sqlalchemy.orm import Session
-from schemas.user_schema import UserAuth, UserResponse
 
 router = APIRouter()
-
-@router.post('/auth', response_model=UserResponse)
-def auth(user_auth: UserAuth, db: Session = Depends(get_db)):
-  token = auth_user(db, user_auth.email)
-  return token
 
 
 @router.get("/{id}")
 def get_user_name(
   id: int,
-  db: Session = Depends(get_db),
-  current_user: User = Depends(get_current_user)
+  db: Session = Depends(get_db)
   ):
   user = get_user(db, id)
   return {"data": f"{user.name}#{str(user.id).zfill(4)}"}
