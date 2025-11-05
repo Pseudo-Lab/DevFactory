4cb08223a9ca801b79d15599a346270e02470b89
hu6r1s <darmaa333@gmail.com>
2025-11-05 19:04:00 +0900

    refactor(getcloser): authentication and authorization using JWT

--- getcloser/backend/app/core/config.py
+++ getcloser/backend/app/core/config.py
@@ -6,8 +6,8 @@ class Settings(BaseSettings):
     """
     JWT 안쓸 것 같아 일단 주석 처리하고 추후 확정 시 삭제
     """
-    # SECRET_KEY: str = os.getenv("SECRET_KEY", "change-me-in-prod")
-    # ALGORITHM: str = "HS256"
-    # ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60"))
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "change-me-in-prod")
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60"))
 
 settings = Settings()
